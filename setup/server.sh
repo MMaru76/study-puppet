ee8c320052d96d9bf36ef6d87a333a45577b858f
marutyan76 <city.mati76@gmail.com>
2018-09-14 16:32:15 +0900

    named add

---
+++ setup/server.sh
@@ -0,0 +1,10 @@
+sed -i s/\"UTC\"/\"Japan\"/ /etc/sysconfig/clock
+ln -sf /usr/share/zoneinfo/Japan /etc/localtime
+sed -i "$ a DNS1=10.0.1.237" /etc/sysconfig/network-scripts/ifcfg-eth0
+vim /etc/sysconfig/network
+
+yum -y install puppet3
+
+yum -y install puppet3-server
+
+cp -f /data/puppet/setup/agent_puppet.conf /etc/puppet/puppet.conf
