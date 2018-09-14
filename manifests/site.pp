37ee7c3e8b083a85e5ed9e66cc77cfe3acde7464
marutyan76 <city.mati76@gmail.com>
2018-09-14 14:34:01 +0900

    named add

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,9 @@
+node default inherits ex_puppet {
+   notify { "woof": message => "Did you define a node? This is the default node message." }
+   include test_page
+}
+
 node ex_puppet {
         include named
 }
+
