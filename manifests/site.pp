75a352cb6eff0aa35fc846a0e6ae7ccdb33deffe
marutyan76 <city.mati76@gmail.com>
2018-09-14 14:32:35 +0900

    named add

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,3 @@
-node default inherits dev_streampack {
-        notify { "woof": message => "Did you define a node? This is the default node message." }
-        include test_page
-}
-
-
 node ex_puppet {
         include named
 }
