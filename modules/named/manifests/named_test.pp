88c17b11f283807f21bf96e5df5aa1e97c9f7388
marutyan76 <city.mati76@gmail.com>
2018-09-14 15:43:18 +0900

    named add

--- modules/named/manifests/named_test.pp
+++ modules/named/manifests/named_test.pp
@@ -1,9 +1,9 @@
 class named::named_test {
 
     file { "test.conf" :
-        path    =>  "/tmp",
+        path    =>  "/tmp/test.conf",
         owner   =>  root,
-        group   =>  named,
+        group   =>  root,
         mode    =>  640,
         source  =>  "puppet://modules/named/na_conf/test.conf"
     }
