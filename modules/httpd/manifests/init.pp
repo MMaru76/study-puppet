4fb1b838eb4904bbd16d480d50f0d0df1958f052
marutyan76 <city.mati76@gmail.com>
2018-09-26 16:56:08 +0900

    named add and mco

--- modules/httpd/manifests/init.pp
+++ modules/httpd/manifests/init.pp
@@ -1,14 +1,14 @@
 class httpd {
 
     case $hostname {
-        'hp' : { include httpd::setup_webdav }
+        'hp','mco' : { include httpd::setup_webdav }
     }
 
     #case $hostname {
     #    'XXX' : { include httpd::setup_22conf }
     #}
 
-    case $hostname {
-        'mco' : { include httpd::setup_24conf }
-    }
+    #case $hostname {
+    #    'XXX' : { include httpd::setup_24conf }
+    #}
 }
